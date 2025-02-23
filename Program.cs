ac4d8e6cc23fd91e025f4f0f37fb64971ead43d5
smileyhead <szijartoleventepal@gmail.com>
2025-02-23 13:58:36 +0100

    Add /flip command
    First implementation. Might elaborate with graphics later.

--- Program.cs
+++ Program.cs
@@ -13,6 +13,7 @@ using System.Globalization;
 using System.Text.Json;
 using System.Linq;
 using DSharpPlus.Commands.Processors.SlashCommands.ArgumentModifiers;
+using System;
 
 namespace Acorn
 {
@@ -598,6 +599,27 @@ namespace Acorn
                 RollTime.Stop();
                 Console.WriteLine($"  Character-generating finished in {RollTime.ElapsedMilliseconds}ms.");
                 if (RollTime.ElapsedMilliseconds > 3000) { PrintDebugMessage($"Generating a character took {RollTime.ElapsedMilliseconds}ms."); }
+            }
+        }
+
+        public class FlipCommand
+        {
+            [Command("flip"), Description("Flips a coin.")]
+            public static async ValueTask ExecuteAsync(CommandContext context)
+            {
+                var FlipTime = System.Diagnostics.Stopwatch.StartNew();
+                Console.WriteLine($"{DateTime.Now.ToString("g", CultureInfo.CreateSpecificCulture("hu-HU"))}: Flipping a coin.");
+
+                Random random = new Random();
+                string answer = "You flip a coin…\n\nIt's ";
+                if (random.NextDouble() >= 0.5) { answer += "heads!"; }
+                else answer += "tails!";
+
+                await context.RespondAsync(answer);
+
+                FlipTime.Stop();
+                Console.WriteLine($"  Coin-flipping finished in {FlipTime.ElapsedMilliseconds}ms.");
+                if (FlipTime.ElapsedMilliseconds > 3000) { PrintDebugMessage($"Flipping a coin took {FlipTime.ElapsedMilliseconds}ms."); }
             }
         }
     }
