dfe74bfee772f3d1220b4070be13648dae01dfb1
smileyhead <szijartoleventepal@gmail.com>
2026-07-01 19:20:13 +0200

    Final implementation of number expansion. Closes #42

--- Classes/NumberExpander.cs
+++ Classes/NumberExpander.cs
@@ -1,14 +1,29 @@
+using System.Globalization;
+using Dangl.Calculator;
+
 namespace Acorn.Classes;
 
 public abstract class NumberExpander
 {
-    public static string Expand(string number) => number.ToLower()
-        .Replace("h",  "00")
-        .Replace("k",  "000")
-        .Replace("m",  "000000")
-        .Replace("b",  "000000000")
-        .Replace("t",  "000000000000")
-        .Replace("q",  "000000000000000")
-        .Replace("qa", "000000000000000000")
-        .Replace("p",  "000000000000000000");
+    public static string? Expand(string number)
+    {
+        number = number.ToLower().Trim();
+
+        string denomination = number.Substring(number.LastIndexOf(number.Last(char.IsDigit)) + 1);
+        number = number.Substring(0, number.LastIndexOf(number.Last(char.IsDigit)) + 1);
+
+        if (!double.TryParse(number, CultureInfo.InvariantCulture, out double inputDouble)) return number;
+
+        return denomination switch
+        {
+            "h" => Calculator.Calculate($"{number}*10^2").Result.ToString(CultureInfo.InvariantCulture),
+            "k" => Calculator.Calculate($"{number}*10^3").Result.ToString(CultureInfo.InvariantCulture),
+            "m" => Calculator.Calculate($"{number}*10^6").Result.ToString(CultureInfo.InvariantCulture),
+            "b" => Calculator.Calculate($"{number}*10^9").Result.ToString(CultureInfo.InvariantCulture),
+            "t" => Calculator.Calculate($"{number}*10^12").Result.ToString(CultureInfo.InvariantCulture),
+            "q" or "qa" => Calculator.Calculate($"{number}*10^15").Result.ToString(CultureInfo.InvariantCulture),
+            "p" => Calculator.Calculate($"{number}*10^18").Result.ToString(CultureInfo.InvariantCulture),
+            _ => number
+        };
+    }
 }
